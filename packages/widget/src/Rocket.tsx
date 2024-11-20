483a8fb1d37fe455e113ae09276fd1c4bee9a5e2
Sebastian Brud <sebastian.brud@tectonicinteractive.com>
2024-11-20 16:08:03 +0100

    Change App to a rocket

--- packages/widget/src/Rocket.tsx
+++ packages/widget/src/Rocket.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
+import './Rocket.css'
+
+function Rocket() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        🚀
       </div>
-      <h1>Vite + React</h1>
+      <h1>Rocket Launcher</h1>
       <div className="card">
+        Click to add fire
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          amount of fire {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
